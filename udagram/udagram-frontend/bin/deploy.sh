a3d4b6ac23d66dc5f125e9dd6ee06d24fd240376
Mohammed159159 <mohammedhany300@gmail.com>
2022-12-09 09:47:22 +0200

    added set_env.sh script to deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,15 @@
 aws s3 cp --recursive --acl public-read ./www s3://randomstring20123412efsd/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://randomstring20123412efsd/
+
+
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv PORT=$PORT
+eb setenv DB_PORT=$DB_PORT
+eb setenv AWS_BUCKET=$AWS_BUCKET
+eb setenv AWS_REGION=$AWS_REGION
+eb setenv AWS_PROFILE=$AWS_PROFILE
+eb setenv JWT_SECRET=$JWT_SECRET
+eb setenv URL=$URL
