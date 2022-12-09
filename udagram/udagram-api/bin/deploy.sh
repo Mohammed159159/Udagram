843e0f15b5596d94ee8f45ec82c8eec238b92f68
Mohammed159159 <mohammedhany300@gmail.com>
2022-12-09 21:21:05 +0200

    fixed apiHost url

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,14 +1,14 @@
 # initializing eb environment and using it to deploy backend
 eb init udagram-api --region us-east-1 --platform node.js && eb list && eb use udagram-api-dev && eb deploy
 # setting eb env vars
-eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
-eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-eb setenv POSTGRES_HOST=$POSTGRES_HOST
-eb setenv POSTGRES_DB=$POSTGRES_DB
-eb setenv PORT=$PORT
-eb setenv DB_PORT=$DB_PORT
-eb setenv AWS_BUCKET=$AWS_BUCKET
-eb setenv AWS_REGION=$AWS_REGION
-eb setenv AWS_PROFILE=$AWS_PROFILE
-eb setenv JWT_SECRET=$JWT_SECRET
-eb setenv URL=$URL
+# eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+# eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+# eb setenv POSTGRES_HOST=$POSTGRES_HOST
+# eb setenv POSTGRES_DB=$POSTGRES_DB
+# eb setenv PORT=$PORT
+# eb setenv DB_PORT=$DB_PORT
+# eb setenv AWS_BUCKET=$AWS_BUCKET
+# eb setenv AWS_REGION=$AWS_REGION
+# eb setenv AWS_PROFILE=$AWS_PROFILE
+# eb setenv JWT_SECRET=$JWT_SECRET
+# eb setenv URL=$URL
