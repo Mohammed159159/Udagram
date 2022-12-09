5c214030bdcce041dff61386550eeecb60eac6fb
Mohammed159159 <mohammedhany300@gmail.com>
2022-12-09 08:06:46 +0200

    deploying frontend

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-nyfex3bi.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://localhost:3000/",
 };
 
 /*
